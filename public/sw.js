cf2c7ec1b3ec82ff00cae72aaa92166d64b00c08
DeviousDerrick <derrickmccabe0@gmail.com>
2026-01-22 21:16:52 -0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(clients.claim()));
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // Allow own server files
+  if (url.pathname === '/' || url.pathname === '/sw.js' || url.pathname.startsWith('/api/')) return;
+
+  // Already proxied?
+  if (url.pathname.startsWith('/ocho/')) return;
+
+  event.respondWith(clients.get(event.clientId).then(client => {
+      if (!client) return fetch(event.request);
+      const clientUrl = new URL(client.url);
+      if (clientUrl.pathname.startsWith('/ocho/')) return fetch(event.request);
+      return fetch(event.request);
+  }));
+});
